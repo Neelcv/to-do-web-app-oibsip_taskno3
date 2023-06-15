8000ce67dfd7bf4ff9e05b7e7c300f965621b74d
neel kamal <neelkamal50416@gmail.com>
2023-06-15 21:52:57 +0530

    to-do-web app

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listcotainer = document.getElementById("list-container");
+
+function addTask(){
+    if (inputBox.value ===''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcotainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listcotainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listcotainer.innerHTML);
+}
+function showTask(){
+    listcotainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
